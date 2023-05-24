e15373f4f171c0850ade6e871048094bd5c15e5c
Mateusz Machaj <mateo.mach@gmail.com>
2023-05-24 20:14:39 +0200

    report structure and more css

--- src/browser.py
+++ src/browser.py
@@ -4,6 +4,6 @@ import logging
 
 
 def open_report() -> None:
-    report = Path("reports", "report.html")
+    report = Path("reports", "report1", "index.html")
     webbrowser.open_new_tab(report)
     logging.info("The newest report has been opened in a browser.")
--- src/report.py
+++ src/report.py
@@ -1,35 +1,68 @@
 import logging
 from pathlib import Path
 import jinja2
+import shutil
+import os
 
 
-def _fill_template(template_str: str) -> str:
-    """Fill the template using Jinja templates. 
+class ReportCreator:
+    def __init__(self) -> None:
+        self.asset_paths = [
+            Path("assets", "static", "style.css"),
+            Path("assets", "static", "images", "top-image.jpg"),
+        ]
+        self.report_result = ""
+        self.export_path = ""
 
-    Args:
-        template_str (str): String report template html file.
+    def _add_asset(self, *path_elems) -> Path:
+        # ! USE THAT
+        path = Path(*path_elems)
+        self.asset_paths.append(path)
+        self.asset_paths = list(set(self.asset_paths))
+        return path
 
-    Returns:
-        str: String result report html file.
-    """
-    environment = jinja2.Environment()
-    template = environment.from_string(template_str)
-    style = Path("assets", "static", "template", "style.css").read_text()
-    context = {"name": "Siema", "style": style}
-    return template.render(context)
+    def _fill_template(self, template_str: str) -> str:
+        """Fill the template using Jinja templates.
 
-def generate() -> str:
-    """Read the template report file, fill the placeholders and save the results
-    in the new report file.
-    """
-    try:
-        template = Path("assets", "static", "template", "areport_template.html").read_text()
-        out_path = Path("reports", "repodt.html")
+        Args:
+            template_str (str): String report template html file.
 
-        result = _fill_template(template)
-        with out_path.open("w", encoding="utf-8") as f:
-            f.write(result)
-        logging.info("New report has been generated.")
-        
-    except FileNotFoundError:
-        logging.error("Some assets were missing. The report could not be generated.")
+        Returns:
+            str: String result report html file.
+        """
+        environment = jinja2.Environment()
+        template = environment.from_string(template_str)
+        context = {"name": "Siema"}
+        return template.render(context)
+
+    def _export_assets(self) -> None:
+        for path in self.asset_paths:
+            shutil.copy(path, self.export_path)
+        index_path = self.export_path.joinpath("index.html")
+        with index_path.open("w", encoding="utf-8") as f:
+            f.write(self.report_result)
+
+    def generate(self) -> str:
+        """Read the template report file, fill the placeholders and save the results
+        in the new report file.
+        """
+        try:
+            template = Path("assets", "static", "report_template.html").read_text(
+                encoding="utf-8"
+            )
+            self.report_result = self._fill_template(template)
+
+            self.export_path = Path("reports", "report1")
+            if not os.path.isdir(self.export_path):
+                os.mkdir(self.export_path)
+            self._export_assets()
+            logging.info("New report has been generated.")
+
+        except FileNotFoundError:
+            logging.error(
+                "Some assets were missing. The report could not be generated."
+            )
+
+
+def generate() -> None:
+    return ReportCreator().generate()
