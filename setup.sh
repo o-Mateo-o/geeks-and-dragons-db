3857c8a0e0522c285b38a6b8bde0624685d81dbe
Mateusz Machaj <mateo.mach@gmail.com>
2023-06-16 03:40:16 +0200

    bash file change

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 pip install virtualenv 				                                # install the virtual environment manager
 # Python path - can be altered down there !!!!!
-~/AppData/Local/Programs/Python/Python39/python.exe -m venv .venv	# create a virtual environment 
+"~/AppData/Local/Programs/Python/Python39/python.exe" -m venv .venv	# create a virtual environment 
 source .venv/Scripts/activate                                       # activate the virtual environment
 pip install --upgrade pip						                    # upgrade pip
 pip install -r requirements.txt			                	        # install dependencies
