68bd691cc9736c5202a564e390976e4331f1fbd8
Mateusz Machaj <mateo.mach@gmail.com>
2023-06-21 23:44:46 +0200

    some fixes in pytania

--- queries/pytania.sql
+++ queries/pytania.sql
@@ -1,15 +1,25 @@
 --1
-SELECT YEAR(s.date) year, MONTH(s.date) month,
+SELECT YEAR(s.date) year,
+    MONTH(s.date) month,
     s.staff_id,
     COUNT(s.sale_id) amount_of_sales,
-    ROW_NUMBER() OVER (PARTITION BY YEAR(s.date), 
-    MONTH(s.date) ORDER BY COUNT(s.sale_id) DESC) AS rank
+    ROW_NUMBER() OVER (
+        PARTITION BY YEAR(s.date),
+        MONTH(s.date)
+        ORDER BY COUNT(s.sale_id) DESC
+    ) AS rank
 FROM sales s
-    GROUP BY YEAR(s.date), MONTH(s.date), s.staff_id
-    ORDER BY YEAR(s.date), MONTH(s.date), ROW_NUMBER() 
-    OVER (PARTITION BY YEAR(s.date), 
-    MONTH(s.date) ORDER BY COUNT(s.sale_id) DESC);
-
+WHERE s.return_oper IS FALSE
+GROUP BY YEAR(s.date),
+    MONTH(s.date),
+    s.staff_id
+ORDER BY YEAR(s.date),
+    MONTH(s.date),
+    ROW_NUMBER() OVER (
+        PARTITION BY YEAR(s.date),
+        MONTH(s.date)
+        ORDER BY COUNT(s.sale_id) DESC
+    );
 --2
 SELECT g.title,
     GROUP_CONCAT(p.particip_id SEPARATOR ", ") player_id
@@ -17,66 +27,67 @@ FROM participations p
     LEFT JOIN tournaments t USING(tournament_id)
     LEFT JOIN games g USING(game_id)
 WHERE p.place >= 1
-AND p.place <= 10
+    AND p.place <= 10
 GROUP BY g.title;
---3
-SELECT g.title,
-    COUNT(s.sale_id) cash
+--3a
+SELECT g.title game,
+    SUM(p.amount) amount
 FROM games g
     LEFT JOIN inventory i USING(game_id)
     LEFT JOIN sales s USING(inventory_id)
     LEFT JOIN payments p USING(payment_id)
 GROUP BY g.title
-ORDER BY COUNT(p.payment_id) DESC;
-
-SELECT g.title,
-    COUNT(r.rental_id) money
+ORDER BY SUM(p.amount) DESC
+LIMIT 5;
+--3b
+SELECT g.title game,
+    SUM(p.amount) amount
 FROM games g
     LEFT JOIN inventory i USING(game_id)
-    LEFT JOIN rental r USING(inventory_id)
+    LEFT JOIN rental s USING(inventory_id)
     LEFT JOIN payments p USING(payment_id)
 GROUP BY g.title
-ORDER BY COUNT(r.rental_id) DESC
+ORDER BY SUM(p.amount) DESC
 LIMIT 5;
-
 --DODATKOWE
 --1
 SELECT gm.game_category,
-    COUNT(r.rental_id) + COUNT(p.payment_id) popularity
+    COUNT(r.rental_id) + COUNT(s.sale_id) popularity
 FROM game_categories gm
     LEFT JOIN games g USING(category_id)
     LEFT JOIN inventory i USING(game_id)
     LEFT JOIN rental r USING(inventory_id)
-    LEFT JOIN payments p USING(payment_id)
+    LEFT JOIN sales s USING(inventory_id)
+WHERE s.return_oper IS FALSE
 GROUP BY gm.game_category
-ORDER BY COUNT(r.rental_id) + COUNT(p.payment_id) DESC
+ORDER BY COUNT(r.rental_id) + COUNT(s.sale_id) DESC
 LIMIT 5;
 --2
-SELECT YEAR(s.date) year, MONTH(s.date) month,
-MAX(p.amount) highest,
-MIN(p.amount) lowest
+SELECT YEAR(s.date) year,
+    MONTH(s.date) month,
+    MAX(p.amount) highest,
+    MIN(p.amount) lowest
 FROM payments p
-LEFT JOIN sales s USING(payment_id)
-GROUP BY YEAR(s.date), MONTH(s.date)
+    LEFT JOIN sales s USING(payment_id)
+GROUP BY YEAR(s.date),
+    MONTH(s.date)
 ORDER BY SUM(p.amount) ASC;
-
 --3
 SELECT FORMAT(CAST(t.start_time AS DATE), 'ddd') name_of_day,
-       COUNT(FORMAT(CAST(t.start_time AS DATE), 'ddd')) amount
+    COUNT(FORMAT(CAST(t.start_time AS DATE), 'ddd')) amount
 FROM tournaments t
 GROUP BY FORMAT(CAST(t.start_time AS DATE), 'ddd')
 ORDER BY COUNT(FORMAT(CAST(t.start_time AS DATE), 'ddd')) DESC
 LIMIT 1;
-
 --4 WPŁYW ILOŚCI RANDEK NA ILOŚĆ SPRZEDANYCH PRODUKTÓW PRZEZ PRACOWNIKA
 SELECT st.first_name,
     st.last_name,
-    r.dates_number,
-    count(sa.sale_id)
+    SUM(r.dates_number) dates_number,
+    COUNT(sa.sale_id) sales_number
 FROM staff st
     LEFT JOIN relationships r USING(staff_id)
     LEFT JOIN sales sa USING(staff_id)
+WHERE sa.return_oper IS FALSE
 GROUP BY st.first_name,
     st.last_name
-ORDER BY count(sa.sale_id) DESC;
-
+ORDER BY COUNT(sa.sale_id) DESC;
